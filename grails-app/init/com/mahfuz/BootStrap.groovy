8f14acac9258bed7523be17261982ff58cdada12
Mahfuz07 <34595383+Mahfuz07@users.noreply.github.com>
2020-07-29 19:32:57 +0600

    Update BootStrap.groovy

--- grails-app/init/com/mahfuz/BootStrap.groovy
+++ grails-app/init/com/mahfuz/BootStrap.groovy
@@ -8,11 +8,11 @@ class BootStrap {
         def USERRole = Role.findOrSaveWhere(authority: 'ROLE_USER')
 
 
-//       def admin = User.findOrSaveWhere(email: 'admin@localhost.local', password: 'admin', firstName: 'admin', lastName: 'Joshi', address: 'Dhaka',phone: '01555555555',birthday: new Date())
-//
-//        if (!admin.authorities.contains(adminRole)) {
-//            UserRole.create(admin, adminRole, true)
-//        }
+       def admin = User.findOrSaveWhere(email: 'admin@localhost.local', password: 'admin', firstName: 'admin', lastName: 'Joshi', address: 'Dhaka',phone: '01555555555',birthday: new Date())
+
+        if (!admin.authorities.contains(adminRole)) {
+            UserRole.create(admin, adminRole, true)
+        }
     }
     def destroy = {
     }
